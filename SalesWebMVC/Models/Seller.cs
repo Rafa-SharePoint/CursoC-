084ebf019da728fc031fd6f73b1c5fb1eada0142
Rafael <rafapalma.i@gmail.com>
2021-02-04 14:30:34 +0000

    Foreignm Key Not null

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 
 namespace SalesWebMVC.Models
 {
@@ -13,6 +14,7 @@ namespace SalesWebMVC.Models
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
